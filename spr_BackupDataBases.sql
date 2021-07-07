e568760504022106f85e2bbd3a38322dee6a7069
Roksolana <60979763+roksolanaabr@users.noreply.github.com>
2021-07-08 00:12:49 +0300

    Update spr_BackupDataBases.sql

--- spr_BackupDataBases.sql
+++ spr_BackupDataBases.sql
@@ -16,23 +16,33 @@ IF OBJECT_ID ( N'[dbo].[spr_backupDataBasesFULL]', N'P' ) IS NOT NULL -- another
 GO 
 
 -- Create a stored procedure that backup dataBases
-CREATE PROCEDURE [dbo].[spr_backupDataBasesFULL] (@dataBase NVARCHAR(250))
+CREATE PROCEDURE [dbo].[spr_backupDataBasesFULL]
 AS BEGIN
 	SET NOCOUNT ON
 
 	BEGIN TRY
 
 		DECLARE @fileName NVARCHAR(250),
+		@dataBaseName NVARCHAR(250),
 		@path NVARCHAR (250),
 		@fileDate NVARCHAR(20)
 		SELECT @fileDate = CONVERT(NVARCHAR(20),GETDATE(),112)
 		SET @path = 'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\Backup\'
+		DECLARE db_cursor CURSOR FOR
+		SELECT name
+		FROM MASTER.dbo.sysdatabases
+		WHERE name NOT IN ('master','model','msdb','tempdb')
+		OPEN db_cursor
+		FETCH NEXT FROM db_cursor INTO @dataBaseName
+		WHILE @@FETCH_STATUS = 0
 		BEGIN
-
-			SET @fileName = @path + @dataBase + '_' + @fileDate + '.BAK'
-			BACKUP DATABASE @dataBase TO DISK = @fileName
+			SET @fileName = @path + @dataBaseName + '_' + @fileDate + '.BAK'
+			BACKUP DATABASE @dataBaseName TO DISK = @fileName
 			WITH NAME = N'FULL Databases Backup'
+			FETCH NEXT FROM db_cursor INTO @dataBaseName
 		END
+		CLOSE db_cursor
+		DEALLOCATE db_cursor
 	END TRY
 	BEGIN CATCH
 		DECLARE @ErrorMessage NVARCHAR(4000)
@@ -41,6 +51,3 @@ AS BEGIN
 	END CATCH
 END
 GO
-
-
-EXEC [dbo].[spr_backupDataBasesFULL] 'Shop LV-608.db'
